446756490648f222745c2a2c7708f4e57370db0e
Vadim Prokoshin <vpro85@gmail.com>
2021-10-10 00:12:12 +0200

    Add Models for User, Post, Comment and Tag

--- main.py
+++ main.py
@@ -1,11 +1,72 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import DevConfig
 
 app = Flask(__name__)
+app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    tags = db.relationship('Tag', secondary=tags, backref=db.backref('posts', lazy='dynamic'))
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
+
+
+tags = db.Table('post_tags',
+                db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+                )
 
 
 @app.route('/')
-def hello():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return '<h1>Hello World!</h1>'
 
 
 if __name__ == '__main__':

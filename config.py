d229717afefdbd809cb0429dc8df8db9cae0060d
Vadim Prokoshin <vpro85@gmail.com>
2021-10-11 12:19:42 +0200

    Change App Structure

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     POSTS_PER_PAGE = 10
 
 
 class ProdConfig(Config):
     SECRET_KEY = ')\x94/\x0b\x9f\xcd\xd6\xe9Cw2\x82\xbfS!\xbf\xc0\x86I\x02\xf0l\xf7\x9e'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 
 
 class DevConfig(Config):
     debug = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_ECHO = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SECRET_KEY = '\xe1Nb.\x08\xaf\x12x|\xe6#ua\xfem\xde\x0f\xcb~\xc9uA:\xda'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
--- manage.py
+++ manage.py
@@ -1,6 +1,11 @@
-from main import app, db, User, Post, Comment, Tag, migrate
+import os
+from webapp import db, migrate, create_app
+from webapp.blog.models import User, Post, Tag
+
+env = os.environ.get('WEBAPP_ENV', 'dev')
+app = create_app('config.%sConfig' % env.capitalize())
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Post=Post, Comment=Comment, Tag=Tag, migrate=migrate)
+    return dict(app=app, db=db, User=User, Post=Post, Tag=Tag, migrate=migrate)
